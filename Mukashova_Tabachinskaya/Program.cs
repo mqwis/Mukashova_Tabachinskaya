8669dd44712ffae0eb3d01e61bd4c586a6b0e177
mukashova.as2958 <mukashova.as2958@C0M31-L1>
2025-10-22 14:25:48 +0500

    Добавьте файлы проекта.

---
+++ Mukashova_Tabachinskaya/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите первое слово: ");
+        string word1 = Console.ReadLine();
+
+        Console.Write("Введите второе слово: ");
+        string word2 = Console.ReadLine();
+
+        string result = word1 + " " + word2;
+
+        Console.WriteLine("Результат: " + result);
+    }
+}
